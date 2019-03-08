de3b4357a2096b3adc6e82c487626ed7bc07cf6a
Youssef Sherif <sharief@aucegypt.edu>
2019-03-08 23:22:53 +0200

    Add boilerplate code

---
+++ src/config/ioc_config.ts
@@ -0,0 +1,10 @@
+import 'reflect-metadata';
+
+import { Container } from 'inversify';
+import { productionValues } from './ioc-container-modules';
+
+const container = new Container({ autoBindInjectable: true });
+
+container.load(productionValues);
+
+export default container;
---
+++ src/main.ts
@@ -0,0 +1,11 @@
+import container from './config/ioc_config';
+import { MainController } from './controllers/main-controller';
+process.on('unhandledRejection', up => {
+    throw up;
+});
+
+const controller = container.get(MainController);
+
+(async function main() {
+    await controller.start();
+})();
