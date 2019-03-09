28fca79531be43a46b0514e21a939a71f72ee26a
Youssef Sherif <sharief@aucegypt.edu>
2019-03-09 09:19:51 +0200

    Remove timestamp from logger

--- src/infrastructure/logger.ts
+++ src/infrastructure/logger.ts
@@ -11,7 +11,7 @@ const l = createLogger({
     new transports.File({ filename: 'error.log', level: 'error' }),
     new transports.File({ filename: 'combined.log' }),
   ],
-  format: format.combine(format.timestamp(), format.splat(), format.simple()),
+  format: format.combine(format.splat(), format.simple()),
 })
 
 if (process.env.NODE_ENV !== 'production') {
